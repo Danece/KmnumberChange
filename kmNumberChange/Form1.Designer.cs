8ba54e211352ebef2b2a2aa311d0cc29da47ff45
Danece <a0938550310@gmail.com>
2020-07-16 15:39:51 +0800

    加入專案檔案。

---
+++ kmNumberChange/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace kmNumberChange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LogTest = new System.Windows.Forms.Label();
+            this.ten_million = new System.Windows.Forms.PictureBox();
+            this.pictureBox17 = new System.Windows.Forms.PictureBox();
+            this.unit = new System.Windows.Forms.PictureBox();
+            this.hundred = new System.Windows.Forms.PictureBox();
+            this.ten = new System.Windows.Forms.PictureBox();
+            this.million = new System.Windows.Forms.PictureBox();
+            this.thousand = new System.Windows.Forms.PictureBox();
+            this.model_9 = new System.Windows.Forms.PictureBox();
+            this.model_8 = new System.Windows.Forms.PictureBox();
+            this.model_7 = new System.Windows.Forms.PictureBox();
+            this.model_6 = new System.Windows.Forms.PictureBox();
+            this.model_5 = new System.Windows.Forms.PictureBox();
+            this.model_4 = new System.Windows.Forms.PictureBox();
+            this.model_3 = new System.Windows.Forms.PictureBox();
+            this.model_2 = new System.Windows.Forms.PictureBox();
+            this.model_1 = new System.Windows.Forms.PictureBox();
+            this.model_0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ten_million)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hundred)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ten)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.million)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thousand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(505, 353);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(100, 22);
+            this.input.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.label1.Location = new System.Drawing.Point(379, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "輸入里程數";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(611, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogTest
+            // 
+            this.LogTest.AutoSize = true;
+            this.LogTest.Location = new System.Drawing.Point(100, 411);
+            this.LogTest.Name = "LogTest";
+            this.LogTest.Size = new System.Drawing.Size(37, 12);
+            this.LogTest.TabIndex = 20;
+            this.LogTest.Text = "LOG : ";
+            // 
+            // ten_million
+            // 
+            this.ten_million.BackColor = System.Drawing.Color.Transparent;
+            this.ten_million.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ten_million.BackgroundImage")));
+            this.ten_million.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ten_million.Location = new System.Drawing.Point(388, 305);
+            this.ten_million.Name = "ten_million";
+            this.ten_million.Size = new System.Drawing.Size(11, 15);
+            this.ten_million.TabIndex = 21;
+            this.ten_million.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            this.pictureBox17.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox17.BackgroundImage")));
+            this.pictureBox17.Location = new System.Drawing.Point(744, 345);
+            this.pictureBox17.Name = "pictureBox17";
+            this.pictureBox17.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox17.TabIndex = 19;
+            this.pictureBox17.TabStop = false;
+            // 
+            // unit
+            // 
+            this.unit.BackColor = System.Drawing.Color.Transparent;
+            this.unit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("unit.BackgroundImage")));
+            this.unit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.unit.Location = new System.Drawing.Point(443, 305);
+            this.unit.Name = "unit";
+            this.unit.Size = new System.Drawing.Size(11, 15);
+            this.unit.TabIndex = 17;
+            this.unit.TabStop = false;
+            // 
+            // hundred
+            // 
+            this.hundred.BackColor = System.Drawing.Color.Transparent;
+            this.hundred.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("hundred.BackgroundImage")));
+            this.hundred.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hundred.Location = new System.Drawing.Point(421, 305);
+            this.hundred.Name = "hundred";
+            this.hundred.Size = new System.Drawing.Size(11, 15);
+            this.hundred.TabIndex = 16;
+            this.hundred.TabStop = false;
+            // 
+            // ten
+            // 
+            this.ten.BackColor = System.Drawing.Color.Transparent;
+            this.ten.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ten.BackgroundImage")));
+            this.ten.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ten.Location = new System.Drawing.Point(432, 305);
+            this.ten.Name = "ten";
+            this.ten.Size = new System.Drawing.Size(11, 15);
+            this.ten.TabIndex = 15;
+            this.ten.TabStop = false;
+            // 
+            // million
+            // 
+            this.million.BackColor = System.Drawing.Color.Transparent;
+            this.million.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("million.BackgroundImage")));
+            this.million.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.million.Location = new System.Drawing.Point(399, 305);
+            this.million.Name = "million";
+            this.million.Size = new System.Drawing.Size(11, 15);
+            this.million.TabIndex = 14;
+            this.million.TabStop = false;
+            // 
+            // thousand
+            // 
+            this.thousand.BackColor = System.Drawing.Color.Transparent;
+            this.thousand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("thousand.BackgroundImage")));
+            this.thousand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.thousand.Location = new System.Drawing.Point(410, 305);
+            this.thousand.Name = "thousand";
+            this.thousand.Size = new System.Drawing.Size(11, 15);
+            this.thousand.TabIndex = 13;
+            this.thousand.TabStop = false;
+            // 
+            // model_9
+            // 
+            this.model_9.Location = new System.Drawing.Point(728, 276);
+            this.model_9.Name = "model_9";
+            this.model_9.Size = new System.Drawing.Size(60, 60);
+            this.model_9.TabIndex = 12;
+            this.model_9.TabStop = false;
+            // 
+            // model_8
+            // 
+            this.model_8.Location = new System.Drawing.Point(611, 276);
+            this.model_8.Name = "model_8";
+            this.model_8.Size = new System.Drawing.Size(60, 60);
+            this.model_8.TabIndex = 11;
+            this.model_8.TabStop = false;
+            // 
+            // model_7
+            // 
+            this.model_7.Location = new System.Drawing.Point(728, 210);
+            this.model_7.Name = "model_7";
+            this.model_7.Size = new System.Drawing.Size(60, 60);
+            this.model_7.TabIndex = 10;
+            this.model_7.TabStop = false;
+            // 
+            // model_6
+            // 
+            this.model_6.BackgroundImage = global::kmNumberChange.Properties.Resources._6;
+            this.model_6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.model_6.Location = new System.Drawing.Point(611, 210);
+            this.model_6.Name = "model_6";
+            this.model_6.Size = new System.Drawing.Size(60, 60);
+            this.model_6.TabIndex = 9;
+            this.model_6.TabStop = false;
+            // 
+            // model_5
+            // 
+            this.model_5.Location = new System.Drawing.Point(728, 144);
+            this.model_5.Name = "model_5";
+            this.model_5.Size = new System.Drawing.Size(60, 60);
+            this.model_5.TabIndex = 8;
+            this.model_5.TabStop = false;
+            // 
+            // model_4
+            // 
+            this.model_4.Location = new System.Drawing.Point(611, 144);
+            this.model_4.Name = "model_4";
+            this.model_4.Size = new System.Drawing.Size(60, 60);
+            this.model_4.TabIndex = 7;
+            this.model_4.TabStop = false;
+            // 
+            // model_3
+            // 
+            this.model_3.BackgroundImage = global::kmNumberChange.Properties.Resources._3;
+            this.model_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.model_3.Location = new System.Drawing.Point(728, 78);
+            this.model_3.Name = "model_3";
+            this.model_3.Size = new System.Drawing.Size(60, 60);
+            this.model_3.TabIndex = 6;
+            this.model_3.TabStop = false;
+            // 
+            // model_2
+            // 
+            this.model_2.Location = new System.Drawing.Point(611, 78);
+            this.model_2.Name = "model_2";
+            this.model_2.Size = new System.Drawing.Size(60, 60);
+            this.model_2.TabIndex = 5;
+            this.model_2.TabStop = false;
+            // 
+            // model_1
+            // 
+            this.model_1.BackgroundImage = global::kmNumberChange.Properties.Resources._1;
+            this.model_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.model_1.Location = new System.Drawing.Point(728, 12);
+            this.model_1.Name = "model_1";
+            this.model_1.Size = new System.Drawing.Size(60, 60);
+            this.model_1.TabIndex = 4;
+            this.model_1.TabStop = false;
+            // 
+            // model_0
+            // 
+            this.model_0.BackgroundImage = global::kmNumberChange.Properties.Resources._0;
+            this.model_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.model_0.Location = new System.Drawing.Point(611, 12);
+            this.model_0.Name = "model_0";
+            this.model_0.Size = new System.Drawing.Size(60, 60);
+            this.model_0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.model_0.TabIndex = 3;
+            this.model_0.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(593, 325);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 433);
+            this.Controls.Add(this.ten_million);
+            this.Controls.Add(this.LogTest);
+            this.Controls.Add(this.pictureBox17);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.unit);
+            this.Controls.Add(this.hundred);
+            this.Controls.Add(this.ten);
+            this.Controls.Add(this.million);
+            this.Controls.Add(this.thousand);
+            this.Controls.Add(this.model_9);
+            this.Controls.Add(this.model_8);
+            this.Controls.Add(this.model_7);
+            this.Controls.Add(this.model_6);
+            this.Controls.Add(this.model_5);
+            this.Controls.Add(this.model_4);
+            this.Controls.Add(this.model_3);
+            this.Controls.Add(this.model_2);
+            this.Controls.Add(this.model_1);
+            this.Controls.Add(this.model_0);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ten_million)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hundred)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ten)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.million)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thousand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.model_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox model_0;
+        private System.Windows.Forms.PictureBox model_1;
+        private System.Windows.Forms.PictureBox model_3;
+        private System.Windows.Forms.PictureBox model_2;
+        private System.Windows.Forms.PictureBox model_5;
+        private System.Windows.Forms.PictureBox model_4;
+        private System.Windows.Forms.PictureBox model_9;
+        private System.Windows.Forms.PictureBox model_8;
+        private System.Windows.Forms.PictureBox model_7;
+        private System.Windows.Forms.PictureBox model_6;
+        private System.Windows.Forms.PictureBox thousand;
+        private System.Windows.Forms.PictureBox million;
+        private System.Windows.Forms.PictureBox hundred;
+        private System.Windows.Forms.PictureBox ten;
+        private System.Windows.Forms.PictureBox unit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox17;
+        private System.Windows.Forms.Label LogTest;
+        private System.Windows.Forms.PictureBox ten_million;
+    }
+}
+
---
+++ kmNumberChange/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kmNumberChange
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String inputText = input.Text;
+            char[] inputArray = inputText.ToCharArray(0, inputText.Length);
+            Array.Reverse(inputArray);
+
+            int lns = inputArray.Length;
+            /* String log = "";
+             for (int i=0; i<lns; i++)
+             {
+                 log = log + "-" + inputArray[i];
+             }*/
+             LogTest.Text = "LOG : " + lns;
+
+            for (int i = 0; i < lns ; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        unit.BackgroundImage = changeBackground(inputArray[i] - '0');
+                        break;
+                    case 1:
+                        ten.BackgroundImage = changeBackground(inputArray[i] - '0');
+                        break;
+                    case 2:
+                        hundred.BackgroundImage = changeBackground(inputArray[i] - '0');
+                        break;
+                    case 3:
+                        thousand.BackgroundImage = changeBackground(inputArray[i] - '0');
+                        break;
+                    case 4:
+                        million.BackgroundImage = changeBackground(inputArray[i] - '0');
+                        break;
+                    case 5:
+                        ten_million.BackgroundImage = changeBackground(inputArray[i] - '0');
+                        break;
+                }
+            }
+        }
+
+        public Image changeBackground (int num)
+        {
+            Image temp = null;
+            switch (num)
+            {
+                case 0:
+                    temp = Properties.Resources._0;
+                    break;
+                case 1:
+                    temp = Properties.Resources._1;
+                    break;
+                /*case 2:
+                    temp = Properties.Resources._2;
+                    break;*/
+                case 3:
+                    temp = Properties.Resources._3;
+                    break;
+                /*case 4:
+                    temp = Properties.Resources._4;
+                    break;
+                case 5:
+                    temp = Properties.Resources._5;
+                    break;*/
+                case 6:
+                    temp = Properties.Resources._6;
+                    break;
+               /* case 7:
+                    temp = Properties.Resources._7;
+                    break;
+                case 8:
+                    temp = Properties.Resources._8;
+                    break;
+                case 9:
+                    temp = Properties.Resources._9;
+                    break;*/
+            }
+
+            return temp;
+        }
+    }
+}
